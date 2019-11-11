1e41fa2b6660c00b2c4c4132e529007be52ed32c
hkd0694 <gksrudehd123@naver.com>
2019-11-11 15:16:02 +0900

    activity_main 추가

--- app/src/main/java/com/example/xylophone/MainActivity.kt
+++ app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.xylophone
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
